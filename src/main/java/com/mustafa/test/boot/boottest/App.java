e8374ca9d3353d5628c091ba850b67b7d55d3e46
Mustafa Tawil <mustafa.tawil@hotmail.com>
2018-08-24 00:08:35 +0300

    Spring Boot - Bootstrappin

---
+++ src/main/java/com/mustafa/test/boot/boottest/App.java
@@ -0,0 +1,27 @@
+package com.mustafa.test.boot.boottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ *
+ */
+
+@SpringBootApplication
+@RestController
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
+    }
+    
+    @RequestMapping(value = "/hi")
+    public String hello() {
+       return "Hello World";
+    }
+}
