f70a934403769267a4bbb31d6d90484739da2aef
Mustafa Tawil <mustafa.tawil@hotmail.com>
2018-08-25 09:45:04 +0300

    test the remote commit

--- src/main/java/com/mustafa/test/boot/boottest/App.java
+++ src/main/java/com/mustafa/test/boot/boottest/App.java
@@ -16,7 +16,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "start the app!" );
         SpringApplication.run(App.class, args);
     }
     
